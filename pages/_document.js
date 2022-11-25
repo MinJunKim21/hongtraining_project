767f691b4462c0d9a9211748b7681b0d90a5f823
MinJunKim21 <jinju8012@gmail.com>
2022-11-25 21:56:58 +0900

    배포 수정중

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>크로플 | 만나기 편한 운동친구를 찾아줄게!</title>
+          {/* <title>크로플 | 만나기 편한 운동친구를 찾아줄게!</title> */}
           <meta
             name="description"
             content="크로플은 신촌연합 대학의 헬스인 쌩초보부터 헬스초고수까지, 서로 시간 맞을 때 같이 운동 할 친구를 연결시켜 드립니다."
